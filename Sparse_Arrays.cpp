5bc181abbac102daeb3ff448763381f98d5e8688
mojahid-belaman <mojahidbelaman@gmail.com>
2021-10-19 01:03:57 +0100

    add function Sparse Arrays

---
+++ Sparse_Arrays.cpp
@@ -0,0 +1,17 @@
+
+std::vector<int> matchingStrings(std::vector<std::string> strings, std::vector<std::string> queries) 
+{
+    std::vector<int> arr;
+    int count = 0;
+    for (size_t i = 0; i < queries.size(); i++)
+    {
+        for (size_t j = 0; j < strings.size(); j++)
+        {
+            if (!queries[i].compare(strings[j]))
+                count++;
+        }
+        arr.push_back(count);
+        count = 0;
+    }
+    return arr;
+}

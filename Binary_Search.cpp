e4453b92e24def6db8fabd71fef24cd3615fd04d
Mojahid Belaman <mbelaman@e2r13p2.1337.ma>
2021-10-28 17:10:43 +0100

    update algo binary search

--- Binary_Search.cpp
+++ Binary_Search.cpp
@@ -4,16 +4,24 @@
 class Solution {
 
 public:
-    int search(std::vector<int>& nums, int target) {
-        for (int i = 0; i < nums.size(); i++)
+
+    int search(std::vector<int>& nums, int target) 
+    {
+        int start = 0;
+        int end = nums.size() - 1;
+        int mid = start + end / 2;
+
+        while (start <= end)
         {
-            if (nums[i] == target)
-            {
-                std::cout << target << " exists in nums and its index is " << i << std::endl;
-                return i;
-            }
+            if (target > nums[mid])
+                start = mid + 1;
+            else if (target < nums[mid])
+                end = mid - 1;
+            else 
+                return mid;
+            mid = (start + end) / 2;
         }
-        std::cout << target << "exists in nums and its index is " << -1 << std::endl;
         return -1;
     }
+    
 };
